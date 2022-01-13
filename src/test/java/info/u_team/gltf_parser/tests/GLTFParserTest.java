75c271ece57b1c173f01582ce2b2b1c941e9d011
HyCraftHD <hycrafthd@live.de>
2022-01-13 03:17:22 +0100

    Refactor tests to be more generically usable

---
+++ src/test/java/info/u_team/gltf_parser/tests/GLTFParserTest.java
@@ -0,0 +1,82 @@
+package info.u_team.gltf_parser.tests;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.nio.ByteBuffer;
+
+import org.junit.jupiter.api.Test;
+
+import info.u_team.gltf_parser.GLTFParser;
+import info.u_team.gltf_parser.generated.gltf.Accessor;
+import info.u_team.gltf_parser.generated.gltf.BufferView;
+import info.u_team.gltf_parser.generated.gltf.GlTF;
+import info.u_team.gltf_parser.generated.gltf.Image;
+
+public abstract class GLTFParserTest {
+	
+	protected abstract GLTFParser parser();
+	
+	@Test
+	public void testParseDoNotThrowException() {
+		final GLTFParser parser = parser();
+		final GlTF gltf = assertDoesNotThrow(parser::parse);
+		
+		assertNotNull(gltf);
+	}
+	
+	@Test
+	public void testGetBuffer() {
+		final GLTFParser parser = parser();
+		final GlTF gltf = assertDoesNotThrow(parser::parse);
+		
+		final ByteBuffer buffer = parser.getData(gltf.getBuffers().get(0));
+		
+		assertNotNull(buffer);
+		assertTrue(buffer.limit() > 0);
+	}
+	
+	@Test
+	public void testGetBufferViews() {
+		final GLTFParser parser = parser();
+		final GlTF gltf = assertDoesNotThrow(parser::parse);
+		
+		for (final BufferView bufferView : gltf.getBufferViews()) {
+			final ByteBuffer buffer = parser.getData(bufferView);
+			
+			assertNotNull(buffer);
+			assertEquals(bufferView.getByteOffset(), buffer.position());
+			assertEquals(bufferView.getByteLength(), buffer.limit() - buffer.position());
+		}
+	}
+	
+	@Test
+	public void testGetAccessors() {
+		final GLTFParser parser = parser();
+		final GlTF gltf = assertDoesNotThrow(parser::parse);
+		
+		for (final Accessor accessor : gltf.getAccessors()) {
+			final int bufferIndex = ((Number) accessor.getBufferView()).intValue();
+			final BufferView bufferView = gltf.getBufferViews().get(bufferIndex);
+			final ByteBuffer buffer = parser.getData(accessor);
+			
+			assertNotNull(buffer);
+			assertEquals(bufferView.getByteOffset() + accessor.getByteOffset(), buffer.position());
+		}
+	}
+	
+	@Test
+	public void testGetImages() {
+		final GLTFParser parser = parser();
+		final GlTF gltf = assertDoesNotThrow(parser::parse);
+		
+		for (final Image image : gltf.getImages()) {
+			final ByteBuffer buffer = parser.getData(image);
+			
+			assertNotNull(buffer);
+			assertTrue(buffer.limit() > 0);
+		}
+	}
+}
--- src/test/java/info/u_team/gltf_parser/tests/SimpleCubeGLTFParserTest.java
+++ src/test/java/info/u_team/gltf_parser/tests/SimpleCubeGLTFParserTest.java
@@ -1,82 +1,30 @@
 package info.u_team.gltf_parser.tests;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import java.nio.ByteBuffer;
-
-import org.junit.jupiter.api.Test;
-
 import info.u_team.gltf_parser.GLTFParser;
-import info.u_team.gltf_parser.generated.gltf.Accessor;
-import info.u_team.gltf_parser.generated.gltf.BufferView;
-import info.u_team.gltf_parser.generated.gltf.GlTF;
-import info.u_team.gltf_parser.generated.gltf.Image;
+import info.u_team.gltf_parser.TestModelResourceLoader;
 
-public abstract class SimpleCubeGLTFParserTest {
+public abstract class SimpleCubeGLTFParserTest extends GLTFParserTest {
 	
-	protected abstract GLTFParser simpleCubeParser();
-	
-	@Test
-	public void testParseSimpleCubeDoNotThrowException() {
-		final GLTFParser parser = simpleCubeParser();
-		final GlTF gltf = assertDoesNotThrow(parser::parse);
+	public static final class Binary extends SimpleCubeGLTFParserTest {
 		
-		assertNotNull(gltf);
-	}
-	
-	@Test
-	public void testSimpleCubeGetBuffer() {
-		final GLTFParser parser = simpleCubeParser();
-		final GlTF gltf = assertDoesNotThrow(parser::parse);
+		private final byte[] simpleCube = TestModelResourceLoader.readModel(TestModelResourceLoader.SIMPLE_CUBE_BIN);
 		
-		final ByteBuffer buffer = parser.getData(gltf.getBuffers().get(0));
-		
-		assertNotNull(buffer);
-		assertTrue(buffer.limit() > 0);
-	}
-	
-	@Test
-	public void testSimpleCubeGetBufferViews() {
-		final GLTFParser parser = simpleCubeParser();
-		final GlTF gltf = assertDoesNotThrow(parser::parse);
-		
-		for (final BufferView bufferView : gltf.getBufferViews()) {
-			final ByteBuffer buffer = parser.getData(bufferView);
-			
-			assertNotNull(buffer);
-			assertEquals(bufferView.getByteOffset(), buffer.position());
-			assertEquals(bufferView.getByteLength(), buffer.limit() - buffer.position());
+		@Override
+		protected GLTFParser parser() {
+			return GLTFParser.fromBinary(simpleCube, 0, simpleCube.length);
 		}
+		
 	}
 	
-	@Test
-	public void testSimpleCubeGetAccessors() {
-		final GLTFParser parser = simpleCubeParser();
-		final GlTF gltf = assertDoesNotThrow(parser::parse);
+	public static final class Json extends SimpleCubeGLTFParserTest {
 		
-		for (final Accessor accessor : gltf.getAccessors()) {
-			final int bufferIndex = ((Number) accessor.getBufferView()).intValue();
-			final BufferView bufferView = gltf.getBufferViews().get(bufferIndex);
-			final ByteBuffer buffer = parser.getData(accessor);
-			
-			assertNotNull(buffer);
-			assertEquals(bufferView.getByteOffset() + accessor.getByteOffset(), buffer.position());
+		private final byte[] simpleCube = TestModelResourceLoader.readModel(TestModelResourceLoader.SIMPLE_CUBE_JSON);
+		
+		@Override
+		protected GLTFParser parser() {
+			return GLTFParser.fromJson(simpleCube, 0, simpleCube.length);
 		}
+		
 	}
 	
-	@Test
-	public void testSimpleCubeGetImages() {
-		final GLTFParser parser = simpleCubeParser();
-		final GlTF gltf = assertDoesNotThrow(parser::parse);
-		
-		for (final Image image : gltf.getImages()) {
-			final ByteBuffer buffer = parser.getData(image);
-			
-			assertNotNull(buffer);
-			assertTrue(buffer.limit() > 0);
-		}
-	}
 }
